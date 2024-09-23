be2c2eb8f9f97ccf854a01d15726e63216a1a280
Yubeen <dlwptlwkr93@naver.com>
2024-09-23 18:35:28 +0900

    feat: 서치바 엔터 적용 및 초기 검색입력값 설정

--- src/app/(with-searchbar)/searchBar.tsx
+++ src/app/(with-searchbar)/searchBar.tsx
@@ -1,3 +1,38 @@
+'use client';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+
 export default function SearchBar() {
-  return <div>Searchbar Layout</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = searchParams.get('q');
+  const [search, setSearch] = useState(params ?? '');
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onClickSearch = () => {
+    router.push(`/search?q=${search}`);
+  };
+  const enterKey = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      onClickSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        className="border border-gray-300 p-2 m-2 rounded"
+        value={search}
+        onChange={onChangeSearch}
+        onKeyUp={enterKey}
+      />
+      <button
+        className="bg-pink-500 text-white py-2 px-4 rounded"
+        onClick={onClickSearch}
+      >
+        검색
+      </button>
+    </div>
+  );
 }
